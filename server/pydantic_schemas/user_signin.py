38ee2b99aa4f32f55301d9920ecf6a7038453bdd
XGhostieX <abdallahnader85@gmail.com>
2026-01-20 20:28:18 +0200

    server signin route

--- server/pydantic_schemas/user_signin.py
+++ server/pydantic_schemas/user_signin.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 
 
-class UserCreate(BaseModel):
-    name: str
+class UserSignin(BaseModel):
     email: str
     password: str
--- server/routes/auth.py
+++ server/routes/auth.py
@@ -3,13 +3,14 @@ import bcrypt
 from fastapi import APIRouter, Depends, HTTPException
 from database import get_db
 from models.user import User
-from pydantic_schemas.user_create import UserCreate
+from pydantic_schemas.user_signin import UserSignin
+from pydantic_schemas.user_signup import UserSignup
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 
-@router.post('/signup')
-def signup_user(user: UserCreate, db: Session = Depends(get_db)):
+@router.post('/signup', status_code = 201)
+def signup_user(user: UserSignup, db: Session = Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
     if user_db:
         raise HTTPException(400, 'User with the same email already exists!')
@@ -17,4 +18,14 @@ def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+    return user_db
+
+@router.post('/signin')
+def signin_user(user: UserSignin, db: Session = Depends(get_db)):
+    user_db = db.query(User).filter(User.email == user.email).first()
+    if not user_db:
+        raise HTTPException(400, 'User with this email dosn\'t exists!')
+    is_match = bcrypt.checkpw(user.password.encode(), user_db.password)
+    if not is_match:
+        raise HTTPException(400, 'Password is incorrect!')
     return user_db
