47dc36902a8a82eaef9ab684610cfc2fd1d48a72
XGhostieX <abdallahnader85@gmail.com>
2026-02-17 11:42:06 +0200

    favorite server logic

--- server/routes/song.py
+++ server/routes/song.py
@@ -4,11 +4,13 @@ import cloudinary
 import cloudinary.uploader
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends, File, Form, UploadFile
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
 from database import get_db
 from middlewares.auth_middleware import auth_middleware
+from models.favorite import Favorite
 from models.song import Song
+from pydantic_schemas.favorite_song import FavoriteSong
 
 router = APIRouter()
 
@@ -49,3 +51,23 @@ def uplaod_song(song: UploadFile = File(...),
 def list_songs(db: Session = Depends(get_db),auth = Depends(auth_middleware)):
     songs = db.query(Song).all()
     return songs
+
+@router.post('/favorite')
+def favorite_song(song: FavoriteSong,
+                db: Session = Depends(get_db),
+                auth = Depends(auth_middleware)):
+    user_id = auth['uid']
+    favorite_song = db.query(Favorite).filter(Favorite.song_id == song.id, Favorite.user_id == user_id).first()
+    if favorite_song:
+        db.delete(favorite_song)
+        db.commit()
+        return {'message': False}
+    else:
+        db.add(Favorite(id = str(uuid.uuid4()), song_id = song.id, user_id = user_id))
+        db.commit()
+        return {'message': True}
+    
+@router.get('/list/favorites')
+def list_favorite_songs(db: Session = Depends(get_db),auth = Depends(auth_middleware)):
+    favorite_songs = db.query(Favorite).filter(Favorite.user_id == auth['uid']).options(joinedload(Favorite.song)).all()
+    return favorite_songs
